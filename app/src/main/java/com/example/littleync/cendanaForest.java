9540e55f9c18f73a39cdeea95ea17d1b61e6e854
dzhou343 <54200426+dzhou343@users.noreply.github.com>
2020-10-01 23:02:20 +0800

    Added functionality to cendana forest button in travel page

--- app/src/main/java/com/example/littleync/cendanaForest.java
+++ app/src/main/java/com/example/littleync/cendanaForest.java
@@ -6,16 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class TravelPage extends AppCompatActivity {
+public class cendanaForest extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_travel_page);
-    }
-
-    public void cendanaForestButton(View view) {
-        Intent intent = new Intent(this, cendanaForest.class);
-        startActivity(intent);
+        setContentView(R.layout.cendana_forest);
     }
 }
