510b2300e90c4b30fb341c27f1355745251187ff
dzhou343 <54200426+dzhou343@users.noreply.github.com>
2020-10-03 22:49:35 +0800

    added armory

--- app/src/main/java/com/example/littleync/TravelActivity.java
+++ app/src/main/java/com/example/littleync/TravelActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class TravelPage extends AppCompatActivity {
+public class TravelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +16,11 @@ public class TravelPage extends AppCompatActivity {
 
     public void cendanaForestButton(View view) {
         Intent intent = new Intent(this, CendanaForestActivity.class);
+        startActivity(intent);
+    }
+
+    public void armoryButton(View view) {
+        Intent intent = new Intent(this, ArmoryActivity.class);
         startActivity(intent);
     }
 }
