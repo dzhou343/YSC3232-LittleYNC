1f58c287315f64d632580c2ba0304974fb53a055
cherryquinnlg <cherryquinnlg@gmail.com>
2020-10-15 22:57:06 +0800

    -Finished the workflow for the a random page (for debugging)

--- app/src/main/java/com/example/littleync/RandomPage.java
+++ app/src/main/java/com/example/littleync/RandomPage.java
@@ -6,12 +6,18 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RandomPage extends AppCompatActivity{
+    TextView interactive_textbox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.random_page_with_interactive_textbox);
-        TextView interactive_textbox = (TextView) findViewById(R.id.text_view_4_random_page);
+        interactive_textbox = (TextView) findViewById(R.id.text_view_4_random_page);
         // By default, stamina is 0 when the activity is created
         String text = interactive_textbox.getText().toString();
+    }
+
+
+    protected void changeText(String s){
+        interactive_textbox.setText(s);
     }
 }
