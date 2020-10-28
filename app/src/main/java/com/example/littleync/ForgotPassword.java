4216c9e8bf70e397818849431bc8d6bdb0055563
Chua Yun Da <yd.chua@u.yale-nus.edu.sg>
2020-10-28 21:02:34 +0800

    bug fix for forgot password using try catch

--- app/src/main/java/com/example/littleync/ForgotPassword.java
+++ app/src/main/java/com/example/littleync/ForgotPassword.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,30 +20,38 @@ public class ForgotPassword extends AppCompatActivity {
 
     private EditText forgotPasswordInput;
     private String emailToReset;
+    private Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forgot_password);
         forgotPasswordInput = findViewById(R.id.forgot_password_input);
+        resetButton = findViewById(R.id.submit_forgot_password);
     }
 
     public void resetPassword(View view) {
-        emailToReset = forgotPasswordInput.getText().toString();
-        FirebaseAuth.getInstance().sendPasswordResetEmail(emailToReset).addOnCompleteListener(
-                new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Log.d("Forgot Email output", "Successfully reset password");
-                            Toast.makeText(getApplicationContext(), "Success. Please check your email", Toast.LENGTH_LONG).show();
-                            ForgotPassword.super.finish();
-                        } else {
-                            forgotPasswordInput.setError(task.getResult().toString());
+        try{emailToReset = forgotPasswordInput.getText().toString();
+            FirebaseAuth.getInstance().sendPasswordResetEmail(emailToReset).addOnCompleteListener(
+                    new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("Forgot Email output", "Successfully reset password");
+                                Toast.makeText(getApplicationContext(), "Success. Please check your email", Toast.LENGTH_LONG).show();
+                                ForgotPassword.super.finish();
+                            } else {
+                                forgotPasswordInput.setError(task.getException().getMessage());
+                                resetButton.clearFocus();
+                            }
                         }
                     }
-                }
-        );
+            );
+        }
+        catch (Exception e) {
+            forgotPasswordInput.setError(e.getMessage());
+            resetButton.clearFocus();
+        }
 
     }
 }
