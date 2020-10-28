8401f74d3d86be99f006bcd72ce557a5603875d8
Chua Yun Da <yd.chua@u.yale-nus.edu.sg>
2020-10-28 21:02:34 +0800

    bug fix for forgot password using try catch

--- app/src/main/java/com/example/littleync/ForgotPassword.java
+++ app/src/main/java/com/example/littleync/ForgotPassword.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
 import android.widget.Button;
-
 import android.widget.EditText;
 import android.widget.Toast;
 
