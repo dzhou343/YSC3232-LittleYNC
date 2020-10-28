b9a22cc99b7f8d7c4dec568dee1f38aad9ca65cb
Chua Yun Da <yd.chua@u.yale-nus.edu.sg>
2020-10-28 20:55:21 +0800

    Added forgot password page

--- app/src/main/java/com/example/littleync/ForgotPassword.java
+++ app/src/main/java/com/example/littleync/ForgotPassword.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
 import android.widget.Button;
+
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -54,6 +56,7 @@ public class ForgotPassword extends AppCompatActivity {
         }
 
     }
+
 }
 
 
