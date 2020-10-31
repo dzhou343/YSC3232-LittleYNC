099f73d1f11f08ac911feaca84dc3e119b2072bf
CHUA YUN DA <yd.chua@u.yale-nus.edu.sg>
2020-10-31 21:12:48 +0800

    Attempt to read location on the travel activity page

--- app/src/main/java/com/example/littleync/TravelActivity.java
+++ app/src/main/java/com/example/littleync/TravelActivity.java
@@ -11,6 +11,7 @@ import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import com.google.android.location.gms.location.FusedLocationProviderClient;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -19,34 +20,10 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 public class TravelActivity extends AppCompatActivity {
 
-    private FusedLocationProviderClient fLC;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.travel_page);
-        fLC = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        fLC.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Checks for the case when there is no location, could be because the user didn't allow access for the user to the phone's location.
-                        if (location != null) {
-                            Log.d("YOUR LAST LOC", location.toString());
-                        }
-                    }
-                });
     }
 
     public void cendanaForestButton(View view) {
