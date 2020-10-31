2680aad6b438a6fad05c737a3eb488ba0605714d
Chua Yun Da <yd.chua@u.yale-nus.edu.sg>
2020-10-31 21:25:23 +0800

    Added Location

--- app/src/main/java/com/example/littleync/TravelActivity.java
+++ app/src/main/java/com/example/littleync/TravelActivity.java
@@ -1,35 +1,52 @@
 package com.example.littleync;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import com.google.android.location.gms.location.FusedLocationProviderClient;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class TravelActivity extends AppCompatActivity {
 
-	private FusedLocationProviderClient fLC;
-	
-	@Override
-	protected void onCreate() {
-		fLC = LocationServices.getFusedLocationProviderClient(this);
-		fLC.getLastLocation()
-        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                // Checks for the case when there is no location, could be because the user didn't allow access for the user to the phone's location.
-                if (location != null) {
-                    Log.d("YOUR LAST KNOWN LOCATION IS AT:", location)
-                }
-            }
-        });
-	}
-	
+    private FusedLocationProviderClient fLC;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.travel_page);
+        fLC = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fLC.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Checks for the case when there is no location, could be because the user didn't allow access for the user to the phone's location.
+                        if (location != null) {
+                            Log.d("YOUR LAST LOC", location.toString());
+                        }
+                    }
+                });
     }
 
     public void cendanaForestButton(View view) {
@@ -43,17 +60,17 @@ public class TravelActivity extends AppCompatActivity {
     }
 
 
-    public void goToMarketplace(View view){
+    public void goToMarketplace(View view) {
         Intent intent = new Intent(this, MarketplaceActivity.class);
         startActivity(intent);
     }
 
-    public void battleButton(View view){
+    public void battleButton(View view) {
         Intent intent = new Intent(this, SagaBattlegroundActivity.class);
         startActivity(intent);
     }
 
-    public void fishButton(View view){
+    public void fishButton(View view) {
         Intent intent = new Intent(this, EcopondActivity.class);
         startActivity(intent);
     }
