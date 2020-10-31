84773198b2aab92578d60da5c5f470c7d6e273c2
CHUA YUN DA <yd.chua@u.yale-nus.edu.sg>
2020-10-31 21:12:48 +0800

    Attempt to read location on the travel activity page

--- app/src/main/java/com/example/littleync/TravelActivity.java
+++ app/src/main/java/com/example/littleync/TravelActivity.java
@@ -5,9 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import com.google.android.location.gms.location.FusedLocationProviderClient;
 
 public class TravelActivity extends AppCompatActivity {
 
+	private FusedLocationProviderClient fLC;
+	
+	@Override
+	protected void onCreate() {
+		fLC = LocationServices.getFusedLocationProviderClient(this);
+		fLC.getLastLocation()
+        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Checks for the case when there is no location, could be because the user didn't allow access for the user to the phone's location.
+                if (location != null) {
+                    Log.d("YOUR LAST KNOWN LOCATION IS AT:", location)
+                }
+            }
+        });
+	}
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
